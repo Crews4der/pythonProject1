beb6184a3a6239980e395b6d4b79e7d94457af0e
Keresztes Istvan <keresztes2000@yahoo.com>
2022-04-16 16:21:49 +0200

    main.py modositas

---
+++ main.py
@@ -0,0 +1,19 @@
+# name = "Robert"
+#
+# print(type(name))
+#
+# print("Szia" + name +'!')
+#
+# print(type(5))
+
+alap = 3
+kitevo = 4
+
+print(type(alap))
+print("az eredmény: " + str(alap ** kitevo))
+
+print(type(alap ** kitevo))
+
+kitevo = 0.5
+print(type(alap ** kitevo))
+print("az eredmény: " + str(round(alap ** kitevo, 2)))
